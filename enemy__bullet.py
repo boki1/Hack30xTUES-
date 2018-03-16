b804e15908ab8913df1039455377e237aae63e99
Violeta Kabadzhova <vkabadzhova@gmail.com>
2018-03-16 22:37:04 +0200

    add enemy's bullet

---
+++ enemy__bullet.py
@@ -0,0 +1,32 @@
+import turtle
+import time
+import enemies
+
+enemyBullet = turtle.Turtle()
+enemyBullet.shape("circle")
+enemyBullet.color("white")
+enemyBullet.speed(0)
+enemyBullet.shapesize(0.5)
+enemyBullet.hideturtle()
+
+bulletSpeed = 20
+
+bulletState = "ready"
+
+def shooting():
+    global bulletState
+    if bulletState == "ready":
+        bulletState = "fire"
+        x = enemy.xcor()
+        y = enemy.ycor()
+        enemyBullet.setposition(x, y)
+        enemyBullet.showturtle()
+
+while True:
+    time.sleep(2)
+    enemy.shooting()
+
+    
+        
+    
+
