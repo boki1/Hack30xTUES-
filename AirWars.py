ea5ae4945aecdca8a0ec89320833d5ca7d7c0e52
boki1 <kristoimenov@gmail.com>
2018-03-17 13:26:39 +0200

    Start button starts the game

--- AirWars.py
+++ AirWars.py
@@ -1,7 +1,6 @@
-import turtle
+import turtle as t
 import math
 import random
-import winsound
 from tkinter import *
 
 mainMenu = Tk()
@@ -15,7 +14,130 @@ def buy():
 
 def runTheProgram():
     mainMenu.destroy()
-    player.showturtle()
+
+    border = t.Turtle()
+    border.speed(0)
+    border.penup()
+    border.setposition(-550, -350)
+    border.color("white")
+    border.pendown()
+    border.pensize(2)
+
+    border.fd(1100)
+    border.lt(90)
+    border.fd(700)
+    border.lt(90)
+    border.fd(1100)
+    border.lt(90)
+    border.fd(700)
+    border.lt(90)
+    border.hideturtle()
+
+    player = t.Turtle()
+    player.shape("square")
+    player.color("purple")
+    player.penup()
+    player.setposition(0, 0)
+    player.setheading(90)
+    player.speed()
+    player.shapesize(1.9)
+
+    playerspeed = 14
+
+    numberOfEnemies = 6
+    enemies = []
+    for i in range(numberOfEnemies):
+        enemies.append(t.Turtle())
+
+    for enemy in enemies:
+        enemy.color("green")
+        # enemy.shape("square")
+        enemy.shape("circle")
+        enemy.penup()
+        enemy.shapesize(2)
+        enemy.speed(3)
+        x = random.randint(-550, 550)
+        y = random.randint(200, 350)
+        enemy.setposition(x, y)
+
+    enemyspeed = 9
+
+    bullet = t.Turtle()
+    bullet.shape("triangle")
+    bullet.color("pink")
+    bullet.penup()
+    bullet.speed()
+    bullet.shapesize(0.5)
+    bullet.hideturtle()
+
+    bulletspeed = 20
+    bulletstate = "ready"
+
+
+    def moveLeft():
+        x = player.xcor()
+        x -= playerspeed
+        if x < - 530:
+            x = - 530
+        player.setx(x)
+
+    def moveRight():
+        x = player.xcor()
+        x += playerspeed
+        if x > 530:
+            x = 530
+        player.setx(x)
+
+    def moveUp():
+        y = player.ycor()
+        y += playerspeed
+        if y > 330:
+            y = 330
+        player.sety(y)
+
+    def moveDown():
+        y = player.ycor()
+        y -= playerspeed
+        if y < -330:
+            y = -330
+        player.sety(y)
+
+
+    def fireBullet():
+        global bulletstate
+        if bulletstate == "ready":
+            bulletstate = "fire"
+            x = player.xcor()
+            y = player.ycor()
+            bullet.setposition(x, y)
+            bullet.showturtle()
+
+    t.listen()
+    t.onkey(moveLeft, "Left")
+    t.onkey(moveUp, "Up")
+    t.onkey(moveRight, "Right")
+    t.onkey(moveDown, "Down")
+
+    while True:
+        for enemy in enemies:
+            enemy.fd(100)
+        if isCollision(player, enemy):
+            for enemy in enemies:
+                enemy.hideturtle()
+                player.hideturtle()
+                bullet.hideturtle()
+                bulletstate = "fire"
+                break
+
+    if bulletstate == "fire":
+        y = bullet.ycor()
+        y += bulletspeed
+        bullet.sety(y)
+
+    if bullet.ycor() > 275:
+        bullet.hideturtle()
+        bulletstate = "ready"
+
 
 buttonExit=Button(mainMenu, justify = LEFT, command = close)
 photobuttonExit=PhotoImage(file="buttonExit.gif")
@@ -32,104 +154,18 @@ photo=PhotoImage(file="buttonStart.gif")
 buttonStart.config(image=photo,width="90",height="50")
 buttonStart.pack(side=BOTTOM)
 
-wn = turtle.Screen()
+"""
+wn = t.Screen()
 wn.setup(1100, 700, 0, 0)
 wn.bgcolor("black")
 wn.title("Space Invaders")
+"""
 
-border = turtle.Turtle()
-border.speed(0)
-border.penup()
-border.setposition(-550, -350)
-border.color("white")
-border.pendown()
-border.pensize(2)
-
-border.fd(1100)
-border.lt(90)
-border.fd(700)
-border.lt(90)
-border.fd(1100)
-border.lt(90)
-border.fd(700)
-border.lt(90)
-border.hideturtle()
-
-player = turtle.Turtle()
-player.shape("square")
-player.color("purple")
-player.penup()
-player.setposition(0, 0)
-player.setheading(90)
-player.speed()
-player.shapesize(1.9)
-
-playerspeed = 14
-
-# Choose the number of enemies
-numberOfEnemies = 6
-# Create an empty list of enemies
-enemies = []
-
-# Add enemies to the list
-for i in range(numberOfEnemies):
-    enemies.append(turtle.Turtle())
-
-for enemy in enemies:
-    enemy.color("green")
-    # enemy.shape("square")
-    enemy.shape("circle")
-    enemy.penup()
-    enemy.shapesize(2)
-    enemy.speed(0)
-    x = random.randint(-550, 550)
-    y = random.randint(200, 350)
-    enemy.setposition(x, y)
-
-enemyspeed = 9
-
-
-
-def moveLeft():
-    x = player.xcor()
-    x -= playerspeed
-    if x < - 530:
-        x = - 530
-    player.setx(x)
-
-
-def moveRight():
-    x = player.xcor()
-    x += playerspeed
-    if x > 530:
-        x = 530
-    player.setx(x)
-
-
-def moveUp():
-    y = player.ycor()
-    y += playerspeed
-    if y > 330:
-        y = 330
-    player.sety(y)
-
-
-def moveDown():
-    y = player.ycor()
-    y -= playerspeed
-    if y < -330:
-        y = -330
-    player.sety(y)
-
-
-
-turtle.listen()
-turtle.onkey(moveLeft, "Left")
-turtle.onkey(moveUp, "Up")
-turtle.onkey(moveRight, "Right")
-turtle.onkey(moveDown, "Down")
-#turtle.onkey(fireBullet, "space")
-
-
+def isCollision(t1, t2):
+    distance = math.sqrt(math.pow(t1.xcor() - t2.xcor(), 2) + math.pow(t1.ycor() - t2.ycor(), 2))
+    if distance < 15:
+        return True
+    else:
+        return False
 
 mainMenu.mainloop()
